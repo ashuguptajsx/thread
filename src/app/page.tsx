91ff27484b272db59e044b24315f50949bc72bcd
ashuguptajsx <ashugupta1403@gmail.com>
2025-04-24 14:35:40 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,9 @@
 "use client"
 
-import { SignInButton } from "@clerk/nextjs";
-import { SignUpButton } from "@clerk/nextjs";
-import { SignedOut } from "@clerk/nextjs";
-import { SignedIn } from "@clerk/nextjs";
-import { UserButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton  mode="modal">
-          <Button>
-            Sign In
-          </Button>
-          
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-      <Button variant={"secondary" }>Click me</Button>
+      <h1>Hello World</h1>
     </div>
   );
 }
