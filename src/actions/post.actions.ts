5283b4d5b8bf2d39e9540d3a624c290acaab14d6
ashuguptajsx <ashugupta1403@gmail.com>
2025-04-25 13:19:16 +0530

    commit

---
+++ src/actions/post.actions.ts
@@ -0,0 +1,26 @@
+"use server";
+import { prisma } from "@/lib/prisma"
+
+import { getDbUserId } from "./user.action";
+import { revalidatePath } from "next/cache";
+
+export async function createPost(content:string, imageUrl:string){
+    try {
+        const userId = await getDbUserId();
+
+        const post  = await prisma.post.create({
+            data:{
+                content,
+                image: imageUrl,
+                authorId: userId
+            }
+        })
+
+
+        revalidatePath("/")
+        return {success:true, post}
+    } catch (error) {
+        console.log("failed to create the post ")
+        return {success:false}
+    }
+}
--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,32 +1,85 @@
-"use client"
+"use client";
 
-import { useUser } from '@clerk/nextjs'
-import  { useState } from 'react'
-import { Card, CardContent } from './ui/card'
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import { Card, CardContent } from "./ui/card";
+import { Textarea } from "./ui/textarea";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
+import { createPost } from "@/actions/post.actions";
+
+
+
+
 
 const CreatePost = () => {
-    const {user} = useUser()
-    const[content, setContent] = useState("");
-    const[imageUrl, setImageUrl] = useState("");
-    const[isPosting, setIsPosting] = useState(false);
-    const[showImageUpload, setShowImageUpload] = useState(false);
+  const { user } = useUser();
+  const [content, setContent] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [isPosting, setIsPosting] = useState(false);
+  const [showImageUpload, setShowImageUpload] = useState(false);
 
 
-    const handleSubmit = async() =>{
 
+  const handleSubmit = async () => {
+    if(!content.trim() && !imageUrl) return;
+    try {
+      await createPost(content, imageUrl)
+    } catch (error) {
+      
     }
-
+  };
 
   return (
-   <Card className='mb-6'>
-    <CardContent className='pt-6'>
-        <div className='space-y-4'>
+    <Card className="mb-6">
+      <CardContent className="pt-6">
+        <div className="space-y-4">
+          <div className="flex space-x-4">
+            <Avatar className="w-10 h-10">
+              <AvatarImage src={user?.imageUrl || ""} />
+            </Avatar>
+            <Textarea
+            placeholder="what are you thinking"
+            className="min-h-[100px] resize-none border-none focus-visible:ring-0 p-0 text-base"
+            />
+          </div>
+          <div className="flex items-center justify-between border-t pt-4">
+            <div className="flex space-x-2">
+              <Button
+                type="button"
+                variant="ghost"
+                size="sm"
+                className="text-muted-foreground hover:text-primary"
+                onClick={() => setShowImageUpload(!showImageUpload)}
+                disabled={isPosting}
+              >
+                <ImageIcon className="size-4 mr-2" />
+                Photo
+              </Button>
+            </div>
+            <Button
+              className="flex items-center"
+              onClick={handleSubmit}
+              disabled={(!content.trim() && !imageUrl) || isPosting}
+            >
+              {isPosting ? (
+                <>
+                  <Loader2Icon className="size-4 mr-2 animate-spin" />
+                  Posting...
+                </>
+              ) : (
+                <>
+                  <SendIcon className="size-4 mr-2" />
+                  Post
+                </>
+              )}
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 
-        </div>    
-    </CardContent>
-     
-   </Card>
-  )
-}
-
-export default CreatePost
+export default CreatePost;
